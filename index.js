ed9e2eafacef90ee726c9944d99629ce719e05d3
byeongdae yu <brianbdyu@gmail.com>
2022-06-21 12:21:18 -0400

    commit tree 9931096d1a9ca164c2a4f27285349317213f599c

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email){
+  const formData = {
+    name: "Brian",
+    email: "brianbdyu@gmail.com",
+  };
+  const configurationObject = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+};
